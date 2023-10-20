0c9dd8299b75319058caf64a2cd5bed40f771fcb
juliebefana <marcusjuliahiller@gmail.com>
2023-10-21 01:30:38 +0200

    working on styles tailwind

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -13,8 +13,8 @@ export class BlogComponent {
 blogPosts: BlogPost[] = [
   {
     id: 1, // Unique identifier
-    title: 'Sample Post 1',
-    thumbnailUrl: 'path-to-image-1.jpg',
+    title: '5 tips to make your house cozier',
+    thumbnailUrl: 'https://i.pinimg.com/originals/bf/07/06/bf0706699b03512e1d21028c85393d55.jpg',
     body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     creationDate: new Date(),
     likes: 0,
@@ -23,8 +23,18 @@ blogPosts: BlogPost[] = [
   },
   {
     id: 2, // Unique identifier
-    title: 'Sample Post 2',
-    thumbnailUrl: 'path-to-image-2.jpg',
+    title: 'Implement hygge into your life',
+    thumbnailUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqv537AhvvteM7mgL1wRh-fWFZzWR-NqqyAA&usqp=CAU',
+    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    creationDate: new Date(),
+    likes: 0,
+    dislikes: 0,
+    comments: [],
+  },
+  {
+    id: 3, // Unique identifier
+    title: 'How to style home office',
+    thumbnailUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfjLZ-26xt4qZsY--8PU-LNYxuaWo_NVaxnw&usqp=CAU',
     body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     creationDate: new Date(),
     likes: 0,
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/**/*.html',
+    './src/**/*.ts',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
 
